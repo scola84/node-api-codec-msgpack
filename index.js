472738e9cd021ac32f52bc9722d981c05ac86d7a
M.C. van den Boogaart <scola@kvdb.name>
2016-05-31 23:47:03 +0200

    Use ES6 modules, with rollup

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const Decoder = require('./lib/decoder');
-const Encoder = require('./lib/encoder');
 const type = 'application/msgpack';
 
-function decoder(options) {
+import { default as Encoder } from './lib/encoder';
+import { default as Decoder } from './lib/decoder';
+
+export const codec = {
+  Decoder,
+  Encoder
+};
+
+export function decoder(options) {
   return {
     type,
     create: () => new Decoder(options)
   };
 }
 
-function encoder(options) {
+export function encoder(options) {
   return {
     type,
     create: () => new Encoder(options)
   };
 }
-
-module.exports = {
-  type,
-  decoder,
-  encoder,
-  Decoder,
-  Encoder
-};
